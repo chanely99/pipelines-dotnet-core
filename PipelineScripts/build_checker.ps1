1faba4dc790bb3f9048e587185843f7ae9b7f8ff
Chanel Young <chanelyoung99@gmail.com>
2020-06-30 11:49:58 -0700

    gated checkin scripts, moved stuff to its own folder too

---
+++ PipelineScripts/build_checker.ps1
@@ -0,0 +1,10 @@
+﻿$build_failed = $false
+
+if($build_failed) {
+    "Build failed, aborting merge"
+    exit 1
+}else{
+    Write-Host "Build succeeded, merging changes"
+}
+
+
---
+++ PipelineScripts/dry_run_merge_cleanup.ps1
@@ -0,0 +1,6 @@
+﻿$dry_run_branch = (git branch --show-current)
+$original_branch = ($dry_run_branch -split 'dry_run_')[1]
+
+git checkout $original_branch
+git branch -D $dry_run_branch
+
